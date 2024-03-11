7b247564c9f3f698018e1886d3a2a1ae3bd90f56
satyam91s <satyamdubey210061@acropolis.in>
2024-03-11 21:59:02 +0530

    structure add

---
+++ src/main/java/com/example/sikhooindia/SikhooIndiaApplication.java
@@ -0,0 +1,14 @@
+package com.example.sikhooindia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikhooIndiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SikhooIndiaApplication.class, args);
+		System.out.println("Application start..");
+	}
+
+}
---
+++ src/test/java/com/example/sikhooindia/SikhooIndiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sikhooindia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SikhooIndiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
